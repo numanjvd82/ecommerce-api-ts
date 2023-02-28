58db3d2665a30c11f28f84267c2c78a9c0631a92
Numan <nomanmani62@gmail.com>
2023-02-28 22:51:30 +0500

    added cors options

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,14 @@ dotenv.config();
 const app = express();
 const port = parseInt(process.env.PORT) || 5000;
 
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
