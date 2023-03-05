9a6b059fddf6d7cc486520053c33cdcbc20d270a
Numan <nomanmani62@gmail.com>
2023-03-05 20:23:16 +0500

    removed keys from the file

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,10 +1,12 @@
 import { v2 } from 'cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dav9niuk4',
-  api_key: process.env.CLOUDINARY_API_KEY || '352819766948764',
-  api_secret:
-    process.env.CLOUDINARY_API_SECRET || 'Hyd_KA2Ul3lp0st-nfYAK02SIMk',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadFileToCloudinary = async (path: string) => {
